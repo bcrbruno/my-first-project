226de2a1d1ad27857991d141ba88909584585a75
Bruno Rodrigues <bruno@Brunos-MacBook-Pro.local>
2022-09-27 14:26:35 -0300

    Meu primeiro projeto usando Vue.js

---
+++ src/components/PrimeiroComponente.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h2>Olá amigos!</h2>
+    <p>Meu nome é {{ nome }}, trabalho na {{ empresa }} como {{ profissao }}</p>
+    <p>Estou estudando algumas funcionalidades. <button v-on:click="reverseMessage">Clique neste botão</button></p>
+  </div>
+</template>
+
+<script>
+  export default {
+      name: 'PrimeiroComponente',
+      data() {
+          return {
+              nome: "Bruno Rodrigues",
+              profissao: "Analista de suporte",
+              empresa: "Vonix Tecnologia"
+          }
+      }
+    }
+</script>
+
+<style>
+
+</style>
